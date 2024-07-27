94cdbd9d70f74354953f2f933602f3246d8f7589
DEBNATH SINHABABU <debnathsinhababu2017@gmail.com>
2024-07-27 21:48:07 +0530

    counter app

--- src/App.js
+++ src/App.js
@@ -5,34 +5,20 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-
-   const [firstname,setfirstname]=useState('')
-   const [lastname,setlastname]=useState('')
-   const [fullname,setfullname]=useState('')
-   function getfullname(e){
-    e.preventDefault()
-    setfullname(`Full Name: ${firstname} ${lastname}`)
-
-   }
+  const [count,setcount]=useState(0)
+  
   return (
     <div className="App">
-       <h1>Full Name Display</h1>
-
-        <form onSubmit={(e)=>getfullname(e)}>
-            <div class="form-group">
-                <label for="firstname">First Name:</label>
-                <input type="text" id="firstname" name="firstname" required value={firstname} onChange={(e)=>setfirstname(e.target.value)}/>
-            </div>
-            <div class="form-group">
-                <label for="lastname">Last Name:</label>
-                <input type="text" id="lastname" name="lastname" required value={lastname} onChange={(e)=>setlastname(e.target.value)}/>
-            </div>
-            <button type="submit">Submit</button>
-           
-        </form>
-        {
-              fullname && <p>{fullname}</p>
-            }
+      <h3>Counter App</h3> 
+      <p>Count: {count}</p> 
+      <div style={{display:'flex',justifyContent:'center'}}>
+        <button onClick={()=>{
+          setcount((prev)=>prev+1)
+        }}>Increment</button>
+        <button onClick={()=>{
+          setcount((prev)=>prev-1)
+        }}>Decrement</button>
+        </div>   
     </div>
   );
 }
