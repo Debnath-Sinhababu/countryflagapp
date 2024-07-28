43deff3bbeacf7127699a22144a7c9ab286fc263
DEBNATH SINHABABU <debnathsinhababu2017@gmail.com>
2024-07-28 16:52:13 +0530

    weather app

--- src/App.js
+++ src/App.js
@@ -5,79 +5,67 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const [tabledata, settabledata]=useState([])
-  const [pageno, setpageno]=useState(1)
-  const [showdata,setshowdata]=useState([])
- 
-  function pagenateddata(data,limit){
-    if(limit<0 || data.length==0){
-      return
+  const [inputval, setinputval]=useState("")
+  const [showweather, setshowweather]=useState([])
+  const [loading, setloading]=useState(false)
+ async function getweatherreport(){
+  setloading(true)
+  try {
+   
+    let response=await axios.get(`http://api.weatherapi.com/v1/current.json?key=7954c4253e51438a9bb104528242807&q=${inputval}&aqi=no`)
+    let data=response.data;
+    setloading(false)
+    let arr=[]
+    let tempobj={
+      title:'Temperature',
+      value:`${data.current.temp_c}°C`
     }
-   let skip=limit*10
-   let filtereddata=data.slice(skip,skip+10)
-   setshowdata(filtereddata)
-  }
- async function gettabledata(){
-    try {
-      let response=await axios.get(`https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json`)
-      settabledata(response.data)
-      pagenateddata(response.data, 0)
-    } catch (error) {
-      console.error(error)
-      window.alert('failed to fetch data')
-      settabledata([])
-      pagenateddata([],0)
+   arr.push(tempobj)
+    let humidobj={
+      title:'Humidity',
+      value:`${data.current.humidity}%`
     }
+    arr.push(humidobj)
+    let conditionobj={
+      title:'Condition',
+      value:data.current.condition.text
+    }
+    arr.push(conditionobj)
+    let windspeed={
+      title:'Wind Speed',
+      value:`${data.current.wind_kph}kph`
+    }
+   arr.push(windspeed)
+   setshowweather(arr)
+  } catch (error) {
+    console.log(error)
+    setshowweather([])
+    setloading(false)
+    window.alert('Failed to fetch weather data')
+
   }
-
- 
- 
-  useEffect(()=>{
-    gettabledata()
-  },[])
-
-  useEffect(()=>{
-    pagenateddata(tabledata, pageno-1)
-  },[pageno])
-  console.log(showdata)
+   
+  }
   return (
     <div className="App">
-       <h2>Employee Data Table</h2>
-    <table>
-      <thead>
-        <tr>
-            <th>ID</th>
-            <th>Name</th>
-            <th>Email</th>
-            <th>Role</th>
-        </tr>
-        </thead>
-        <tbody>
-        {
-         showdata.length && showdata?.map((obj)=>(
-            <tr>
-            <td>{obj.id}</td>
-            <td>{obj.name}</td>
-            <td>{obj.email}</td>
-            <td>{obj.role}</td>
-            </tr>
-          ))
-        }
-        </tbody>
-    </table>
-    <div style={{display:'flex',justifyContent:'center', marginTop:20}}>
-     <button onClick={()=>{
-      if(pageno>1){
-       setpageno((prev)=>prev-1)
+      <div>
+      <input type='text' onChange={(e)=>setinputval(e.target.value)} value={inputval}/>
+      <button onClick={getweatherreport}>Search</button>
+      </div>
+      {
+        loading? <p>Loading data...</p>:
+      
+      <div className='weather-cards' style={{display:'flex', justifyContent:'center'}}>
+      {
+       showweather.length>0 && showweather.map((obj)=>(
+         <div className='weather-card' style={{margin:20}}>
+          <h3>{obj.title}</h3>
+          <p>{obj.value}</p>
+         </div>
+       ))
       }
-     }}>Previous</button>
-     <button>{pageno}</button>
-     <button onClick={()=>{
-      if(pageno<5){
-       setpageno((prev)=>prev+1)
-      }
-     }}>Next</button>
-    </div>
+      </div>
+}
     </div>
   );
 }
